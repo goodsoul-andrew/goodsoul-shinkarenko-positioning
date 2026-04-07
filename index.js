b072cea5f559482eec95f3d3b3991aef063b5890
Andrew Goodman <goodsoul.andrew@mail.ru>
2026-04-07 19:33:37 +0500

    task4

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ const overlay = document.getElementById('overlay');
 const progressFill = document.getElementById('progressFill');
 const progressTextWhite = document.getElementById('progressTextWhite');
 
+let currentStep = 0;
+const seconds = 3;
+const totalSteps = 60 * seconds;
+
 openModalBtn.addEventListener('click', () => {
     overlay.classList.remove('hidden');
+    const interval = setInterval(() => {
+        currentStep++;
+        const progress = (currentStep / totalSteps) * 100;
+        
+        progressFill.style.width = progress + '%';
+        progressTextWhite.style.width = progress + '%';
+        
+        if (currentStep >= totalSteps) {
+            clearInterval(interval);
+        }
+    }, 1000 / 60);
 });
 
 closeModalBtn.addEventListener('click', () => {
     overlay.classList.add('hidden');
+    currentStep = 0;
 });
-
-const progress = 45;
-
-progressFill.style.width = progress + '%';
-progressTextWhite.style.width = progress + '%';
